f725410239b079f78ab3f9be99dda4fcf8a18959
xnubber <aabbccdd10902@gmail.com>
2022-01-23 17:00:30 +0800

    feat; add authenticator feature

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(session({
 
 const usePassport = require('./config/passport')
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  // res.locals.success_msg = req.flash('success_msg')
+  // res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 
 
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,6 +5,19 @@ const db = require('../../models')
 const Todo = db.Todo
 
 
+// new
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  // const userId = req.user.id
+  console.log(req.user)
+  res.send(req.user)
+})
+
+
+
 // show
 router.get('/:id', (req, res) => {
   const id = req.params.id
